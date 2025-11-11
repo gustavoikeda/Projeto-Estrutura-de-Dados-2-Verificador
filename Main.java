745d4ccbc0db1e91454cd6feb77b1d4fb145ded6
Gustavo Kiyoshi Ikeda <gustavokikeda@gmail.com>
2025-11-10 22:03:47 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,33 @@
+public class Main
+{
+    public static void main(String[] args) {
+        /*Hash h = new Hash(20);
+        NoHash p1 = new NoHash(1, "gato");
+        NoHash p2 = new NoHash(1, "gato");
+        h.insersao_Dispersao_Dupla(p1);
+        h.insersao_Dispersao_String(p2);
+        h.exibir();
+        String oi = "testando";
+        String tchau = "falou";
+        Resultado r = new Resultado(1);
+        Resultado r2 = new Resultado(2);
+
+        r.inserir_novo_documento(oi);
+        r2.inserir_novo_documento(tchau);
+        r.imprimir();
+        r2.imprimir();
+        ArvoreAVL arvore = new ArvoreAVL();
+
+        arvore.inserir(new Resultado("doc1", "doc2", 70));
+        arvore.inserir(new Resultado("doc3", "doc4", 55));
+        arvore.inserir(new Resultado("doc5", "doc6", 40));
+        arvore.inserir(new Resultado("doc5", "doc6", 85));
+        arvore.inserir(new Resultado("doc5", "doc6", 90));
+        arvore.inserir(new Resultado("doc5", "doc6", 72));
+        arvore.inserir(new Resultado("doc5", "doc6", 42));
+        arvore.inserir(new Resultado("doc67", "doc69", 70));// mesma similaridade
+
+        arvore.exibirEmOrdem();
+        System.out.println("Rotações realizadas: " + arvore.getRotacoes());*/
+    }
+}
