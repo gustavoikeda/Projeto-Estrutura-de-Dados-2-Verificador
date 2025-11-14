55bce13858916049330f3709d0f163d746c59dfa
Gustavo Kiyoshi Ikeda <gustavokikeda@gmail.com>
2025-11-13 22:07:43 -0300

    Update NoAVL.java

--- NoAVL.java
+++ NoAVL.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
 public class NoAVL {
-    float valor;
+    double valor;
     ArrayList<Resultado> documentos_similaridade_iguais;
     NoAVL dir;
     NoAVL esq;
     int altura;
 
-    public NoAVL(float similaridade, Resultado resultado){
+    public NoAVL(double similaridade, Resultado resultado){
         this.valor = similaridade;
         this.documentos_similaridade_iguais = new ArrayList<>();
         this.documentos_similaridade_iguais.add(resultado);
@@ -15,3 +15,5 @@ public class NoAVL {
         this.esq = null;
         this.altura = 1;
     }
+
+}
